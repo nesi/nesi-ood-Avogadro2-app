5f0de294438e22cd0ca0b284938e63c79800c5c8
geoffreyweal <geoffrey.weal@gmail.com>
2026-03-28 16:06:54 +1300

    update version of the NeSI OOD Avogadro2 App

--- template/desktops/xfce.sh
+++ template/desktops/xfce.sh
@@ -70,13 +70,22 @@ export LIBGL_ALWAYS_SOFTWARE=1
 
 module load ${xtb_module}
 module load ${amber_module}
-module load ${avogadro2_module}
+#module load ${avogadro2_module}
 
 module use /nesi/nobackup/nesi99999/geoffreyweal/Installations/Avogradro2/modules/all
-module load glibc/2.35-GCCcore-11.3.0
-#module load Avogadro2/1.103.0-foss-2022a
+#module load glibc/2.35-GCCcore-11.3.0
+#module load Avogadro2/1.103.0-linux-x86_64
+module load Avogadro2/1.103.0-foss-2022a
 
 # Launch Avogadro2
-avogadro2.app
-#export QT_DEBUG_PLUGINS=1
-#avogadro2
+#avogadro2.app
+
+# -----------------------------------------------------------------------------
+# 4. FIX 3D RENDERING (Force CPU-based OpenGL software rendering)
+# -----------------------------------------------------------------------------
+export LIBGL_ALWAYS_SOFTWARE=1
+export GALLIUM_DRIVER=llvmpipe
+export QT_OPENGL=software
+export QT_XCB_FORCE_SOFTWARE_OPENGL=1
+
+avogadro2
