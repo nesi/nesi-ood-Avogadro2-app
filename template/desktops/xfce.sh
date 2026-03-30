48087aa84367d2e940f6aa9f536e3d25f81fb1df
geoffreyweal <geoffrey.weal@gmail.com>
2026-03-30 16:20:01 +1300

    update version of the NeSI OOD Avogadro2 App

--- template/desktops/xfce.sh
+++ template/desktops/xfce.sh
@@ -57,7 +57,7 @@ xfconf-query -c xfwm4 -p /general/use_compositing -t bool -s true
 xfconf-query -c xfwm4 -p /general/button_layout -s "|" 2>/dev/null
 
 # START THE WINDOW MANAGER COMPONENTS IN THE BACKGROUND
-#xfwm4 --compositor=off --sm-client-disable &
+xfwm4 --compositor=off --sm-client-disable &
 #xsetroot -solid "#D3D3D3" &
 #xfsettingsd --sm-client-disable &
 #xfce4-panel --sm-client-disable &
